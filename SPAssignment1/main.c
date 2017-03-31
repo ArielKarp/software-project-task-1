1a8a0e809e8ce2aaa0e6bb93827e33e62b32d04a
Ariel <akarpilovsy@gmail.com>
2017-03-31 11:07:45 +0300

    Main & gitignore

--- SPAssignment1/main.c
+++ SPAssignment1/main.c
@@ -6,4 +6,41 @@
 
 
 
+void printIntArr(int* arr) {
+	int i = 0;
+	printf("\nArr:\n");
+	for (; i < 32; ++i) {
+		printf("%d ", arr[i]);
+	}
+}
+
+void printCharArr(char* arr) {
+	int i = 0;
+	printf("\nArr:\n");
+	for (; i < 3; ++i) {
+		printf("%c", arr[i]);
+	}
+}
+
+int main() {
+
+
+	printf("Enter base: ");
+	scanf("%d", &arr[0]);
+	getchar();
+
+	printf("Enter numbers in: ");
+
+	printf("Number is is: %d\n", i);
+
+	char dict[] = {'0', '1') // 1-F
+	c = getchar();
+	while (c != '\n' && c != EOF) {
+		if (isNotInDict(c)) {
+			// handle exit
+		}
+		c = getchar();
+	}
+
+}
 
